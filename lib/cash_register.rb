8e78634f868dbd157b1874c895a564070348e824
github username <github email address>
2020-03-10 22:13:45 -0400

    commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
+class Cash_Register 
+    attr_reader :discount, :items 
+    attr_accessor :total 
 
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount 
+        @items = []
+        @prev = 0
+    end 
+    
+
+end 
